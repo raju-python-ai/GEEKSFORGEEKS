c18edc503b63f8cbbf4e09fb786eda082fa1db76
raju-python-ai <rajasekar012004@gmailcom>
2025-03-05 20:27:21 +0530

    changed my python files

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+# Input: n = 15
+# Output: false
+# Explanation: The number is not divisible by 2 class Solution:
+def isEven (self, n):
+        # code here 
+        if n % 2 ==0:
+           return True
+        else:
+            return False
