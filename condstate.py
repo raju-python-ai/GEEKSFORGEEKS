f69f3a19f7e0cb1325bb51bdc67452cfcd16bd8d
raju-python-ai <rajasekar012004@gmailcom>
2025-03-16 22:14:38 +0530

    changed my python files

---
+++ condstate.py
@@ -0,0 +1,7 @@
+def friends_in_trouble(j_angry, s_angry):
+    if j_angry and s_angry:  
+        return True
+    elif not j_angry and not s_angry:  
+        return True
+    else:  
+        return False
